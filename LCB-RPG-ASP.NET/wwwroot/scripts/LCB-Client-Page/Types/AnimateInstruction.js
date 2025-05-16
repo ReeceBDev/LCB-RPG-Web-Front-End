bf7cb9b46aa73fa0db11cf5fc2da26c96cf9802e
ReeceBDev <reece.bowen99@gmail.com>
2025-05-16 23:17:07 +0100

    Added AnimateInstruction enum which holds animation instructions such as Spin. These are applied later to a list of target objects.

---
+++ LCB-RPG-ASP.NET/wwwroot/scripts/LCB-Client-Page/Types/AnimateInstruction.js
@@ -0,0 +1,5 @@
+export var AnimateInstruction;
+(function (AnimateInstruction) {
+    AnimateInstruction[AnimateInstruction["Spin"] = 0] = "Spin";
+})(AnimateInstruction || (AnimateInstruction = {}));
+//# sourceMappingURL=AnimateInstruction.js.map
