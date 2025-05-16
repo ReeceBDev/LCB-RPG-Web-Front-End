a8151cba4c118058dc87494d9b8efc34f3768893
ReeceBDev <reece.bowen99@gmail.com>
2025-05-16 23:13:28 +0100

    Added new enum PostProcessingInstruction. This holds post processing instructions like weak bloom, strong bloom. These can be assigned to object groups in order to apply the effects.

---
+++ LCB-RPG-ASP.NET/wwwroot/scripts/LCB-Client-Page/Types/PostProcessingInstruction.js
@@ -0,0 +1,8 @@
+export var PostProcessingInstruction;
+(function (PostProcessingInstruction) {
+    PostProcessingInstruction[PostProcessingInstruction["Bloom.Weak"] = 0] = "Bloom.Weak";
+    PostProcessingInstruction[PostProcessingInstruction["Bloom.Middling"] = 1] = "Bloom.Middling";
+    PostProcessingInstruction[PostProcessingInstruction["Bloom.Strong"] = 2] = "Bloom.Strong";
+    PostProcessingInstruction[PostProcessingInstruction["Bloom.VeryStrong"] = 3] = "Bloom.VeryStrong";
+})(PostProcessingInstruction || (PostProcessingInstruction = {}));
+//# sourceMappingURL=PostProcessingInstruction.js.map
---
+++ LCB-RPG-ASP.NET/wwwroot/scripts/LCB-Client-Page/Types/PostProcessingInstruction.ts
@@ -0,0 +1,6 @@
+﻿export enum PostProcessingInstruction{
+        "Bloom.Weak", //Do not define bloom intensities here - consider what happens if an instruction requires multiple values!
+        "Bloom.Middling",
+        "Bloom.Strong",
+        "Bloom.VeryStrong"
+}
